6d860a8c13b2ab8689f44ccc8c725186adfbf3e3
Aaron Pierce <pierceaaron424@gmail.com>
2018-02-28 00:00:32 -0600

    Fixed SUPER broken ship movement

--- FinalProject/src/columbus/PlayerShip.java
+++ FinalProject/src/columbus/PlayerShip.java
@@ -15,7 +15,7 @@ public class PlayerShip implements ShipInterface {
 		
 		//Sets the location of the ship to (1, 1) every time
 		//TODO set a random location for the ship
-		location.setLocation(1, 1);
+		location = new Point(1, 1);
 	}
 	
 	public Point getShipLocation() {
@@ -27,7 +27,7 @@ public class PlayerShip implements ShipInterface {
 		//Checks if the ship is not already at the top edge of the map
 		if(location.y > 0) {
 			//Checks if the space above is a water square
-			if(map[location.x + 1][location.y] < 1) {
+			if(map[location.x][location.y - 1] == 0) {
 				//Moves the ship 1 square north
 				location.y--;
 			}
@@ -36,9 +36,9 @@ public class PlayerShip implements ShipInterface {
 
 	public void moveEast() {
 		//Checks if the ship is not already at the right edge of the map
-		if(location.x < oceanMap.dimension + 1) {
+		if(location.x < oceanMap.dimension - 1) {
 			//Checks if the space to the right is a water square
-			if(map[location.x + 1][location.y] < 1) {
+			if(map[location.x + 1][location.y] == 0) {
 				//Moves the ship 1 square east
 				location.x++;
 			}
@@ -49,7 +49,7 @@ public class PlayerShip implements ShipInterface {
 		//Checks if the ship is not already at the bottom edge of the map
 		if(location.y < oceanMap.dimension - 1) {
 			//Checks if the space below is a water square
-			if(map[location.x + 1][location.y] < 1) {
+			if(map[location.x][location.y + 1] == 0) {
 				//Moves the ship 1 square south
 				location.y++;
 			}
@@ -60,10 +60,14 @@ public class PlayerShip implements ShipInterface {
 		//Checks if the ship is not already at the left edge of the map
 		if(location.x > 0) {
 			//Checks if the space to the left is a water square
-			if(map[location.x + 1][location.y] < 1) {
+			if(map[location.x - 1][location.y] == 0) {
 				//Moves the ship 1 square west
 				location.x--;
 			}
 		}
+	}
+	
+	public String getType() {
+		return "player";
 	}
 }
