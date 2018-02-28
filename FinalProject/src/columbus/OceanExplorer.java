343f1c9019de8b57fdcd76e6836d83c83a6a8dd1
jkash1 <rawrdpro@gmail.com>
2018-02-27 18:46:23 -0600

    Added DrawMap to OceanExplorer--creates grid

--- FinalProject/src/columbus/OceanExplorer.java
+++ FinalProject/src/columbus/OceanExplorer.java
@@ -3,20 +3,24 @@ package columbus;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class OceanExplorer extends Application{
 
 	public final int scale = 40;
 	final int dimensions = 40;
+	Image shipImage, pirateImage, IslandImage;
+	ImageView shipImageView;
 	OceanMap oceanMap;
 	Scene scene;
 	AnchorPane root;
-
-	
-	
+	ShipInterface ship;
 	
 	
 	
@@ -32,11 +36,30 @@ public class OceanExplorer extends Application{
 		oceanStage.setTitle("Columbus vs. the Deep Blue");
 		
 		oceanStage.show();
+		drawMap();
 		
 	//	startSailing();
 		
 	}
-
+	//TODO Load Images
+	
+	/**
+	 * Draws the Grid
+	 */
+	public void drawMap() {
+		for(int x = 0; x < dimensions; x++) {
+			for(int y = 0; y < dimensions; y++) {
+				Rectangle rect = new Rectangle(x*scale, y*scale,scale,scale);
+				rect.setStroke(Color.TURQUOISE);
+				rect.setFill(Color.PALETURQUOISE);
+				root.getChildren().add(rect);
+			
+			}
+		}	
+	}
+	
+	
+	
 	
 	/*
 	 * Main launches the game
