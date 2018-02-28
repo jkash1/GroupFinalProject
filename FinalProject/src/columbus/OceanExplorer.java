9349fc9fd8a7136c45abb07b7f17b4b48022a5be
Aaron Pierce <pierceaaron424@gmail.com>
2018-02-28 00:20:25 -0600

    Added ship image loading

--- FinalProject/src/columbus/OceanExplorer.java
+++ FinalProject/src/columbus/OceanExplorer.java
@@ -25,6 +25,7 @@ public class OceanExplorer extends Application{
 	AnchorPane root;
 	ShipInterface ship;	
 	LinkedList<ShipInterface> pirates = new LinkedList<ShipInterface>();
+	LinkedList<ImageView> pirateImages = new LinkedList<ImageView>();
 	
 	@Override
 	public void start(Stage oceanStage) throws Exception {
@@ -33,19 +34,53 @@ public class OceanExplorer extends Application{
 		dimensions = oceanMap.getDimension();
 		map = oceanMap.getMap();
 		
+		ship = new PlayerShip();
+		shipImageView = createShipImage(ship);
+		
 		//Creates a pane, scene, and draws the map
 		root = new AnchorPane();
 		scene = new Scene(root, 900, 900);
 		drawMap();
+		root.getChildren().add(shipImageView);
 		
 		//Sets the scene, adds a title to the window, and shows it
 		oceanStage.setScene(scene);
 		oceanStage.setTitle("Columbus vs. the Deep Blue");
 		oceanStage.show();
 		
-		//startSailing();
+		//Start listening for user input and moving the boat
+		startSailing();
 	}
 	//TODO Load Images
+	
+	public void startSailing() {
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent ke) {
+				switch(ke.getCode()) {
+				case RIGHT:
+					ship.moveEast();
+					break;
+				case LEFT:
+					ship.moveWest();
+					break;
+				case UP:
+					ship.moveNorth();
+					break;
+				case DOWN:
+					ship.moveSouth();
+					break;
+				case Q:
+					System.exit(0);
+				default:
+					break;
+				}
+				
+				System.out.println(String.format("Ship is at (%d, %d)", ship.getShipLocation().x, ship.getShipLocation().y));
+				
+				updateShips();
+			}
+		});
+	}
 	
 	/**
 	 * Draws the Grid
@@ -71,6 +106,10 @@ public class OceanExplorer extends Application{
 					drawIsland(x, y, "pirate");
 			}
 		}	
+	}
+	
+	public void drawShip(String type) {
+		
 	}
 	
 	public void drawIsland(int x, int y, String type) {
@@ -99,6 +138,41 @@ public class OceanExplorer extends Application{
 		root.getChildren().add(islandImageView);
 	}
 	
+	public void createPirate(int x, int y) {
+		
+	}
+	
+	public ImageView createShipImage(ShipInterface ship) {
+		//Gets the ship type and creates a string for the file name
+		String type = ship.getType();
+		String fileName = null;
+		
+		//Decides which island image to use based on the type variable
+		switch(type) {
+		case("player"):
+			fileName = "/images/ship.png";
+			break;
+		case("pirate"):
+			fileName = "/images/pirateShip.png";
+			break;
+		}
+		
+		//Creates an image and image view with the given file name
+		Image shipImage = new Image(fileName, scale, scale, false, false);
+		ImageView shipImageView = new ImageView(shipImage);
+		
+		//Sets the image views location to the location of the ship
+		shipImageView.setX(ship.getShipLocation().x * scale);
+		shipImageView.setY(ship.getShipLocation().y * scale);
+		
+		//Returns the ship image view
+		return shipImageView;
+	}
+	
+	public void updateShips() {
+		shipImageView.setX(ship.getShipLocation().x * scale);
+		shipImageView.setY(ship.getShipLocation().y * scale);
+	}
 	
 	/*
 	 * Main launches the game
@@ -107,4 +181,4 @@ public class OceanExplorer extends Application{
 		launch(args);
 	}
 
-}
+} //testing Git commit -Antonio
