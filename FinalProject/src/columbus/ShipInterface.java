9875c7ac07bc4d73adfc941f88bd026efb3f56be
Aaron Pierce <pierceaaron424@gmail.com>
2018-02-28 00:31:47 -0600

    Added getShipLocation and getType to interface

--- FinalProject/src/columbus/ShipInterface.java
+++ FinalProject/src/columbus/ShipInterface.java
@@ -1,9 +1,13 @@
 package columbus;
 
+import java.awt.Point;
+
 public interface ShipInterface {
 	//Provides methods for moving each direction
 	public void moveNorth();
 	public void moveEast();
 	public void moveSouth();
 	public void moveWest();
+	public Point getShipLocation();
+	public String getType();
 }
