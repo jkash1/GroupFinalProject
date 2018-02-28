b688a0d2bb01bd3f7677445a66c6fdb86a2cb8ee
Aaron Pierce <pierceaaron424@gmail.com>
2018-02-27 18:02:37 -0600

    Created interface for ships

---
+++ FinalProject/src/columbus/ShipInterface.java
@@ -0,0 +1,9 @@
+package columbus;
+
+public interface ShipInterface {
+	//Provides methods for moving each direction
+	public void moveNorth();
+	public void moveEast();
+	public void moveSouth();
+	public void moveWest();
+}
