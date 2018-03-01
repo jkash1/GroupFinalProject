68e37d3734ca9d3d2723c53c81b55c06a5678b48
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-03-01 01:39:26 -0600

    Simple unit test case succeeds
    A case that tests Pirate Ship movement is currently passing. I am expanding it to test more of the movement.

--- FinalProject/src/testCases/PirateShipTest.java
+++ FinalProject/src/testCases/PirateShipTest.java
@@ -2,13 +2,23 @@ package testCases;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+
 import org.junit.Test;
 
 public class PirateShipTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void PirateShipMoveTest() {
+		columbus.PirateShip pirate = new columbus.PirateShip(0,0);
+		pirate.moveEast();
+		Point loc = pirate.getShipLocation();
+		assertEquals(loc, new Point(1,0));
 	}
 
 }
