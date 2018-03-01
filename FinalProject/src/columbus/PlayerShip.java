118b4cef3bca64ced5ced70d9a570186db3ab21b
Aaron Pierce <pierceaaron424@gmail.com>
2018-03-01 15:25:28 -0600

    Player ship now starts in a random position toward the left of the map

--- FinalProject/src/columbus/PlayerShip.java
+++ FinalProject/src/columbus/PlayerShip.java
@@ -2,6 +2,7 @@ package columbus;
 
 import java.awt.Point;
 import java.util.Observable;
+import java.util.Random;
 
 public class PlayerShip extends Observable implements ShipInterface{
 	//Creates global variables for oceanMap, map array, and location
@@ -16,9 +17,18 @@ public class PlayerShip extends Observable implements ShipInterface{
 		map = oceanMap.getMap();
 		status = new Status();
 		
+		Random rand = new Random();
+		
 		//Sets the location of the ship to (1, 1) every time
 		//TODO set a random location for the ship
-		location = new Point(1, 1);
+		while(location == null) {
+			int x = rand.nextInt(oceanMap.getDimension() / 10);
+			int y = rand.nextInt(oceanMap.getDimension());
+			
+			if(map[x][y] == 0) {
+				location = new Point(x, y);
+			}
+		}
 	}
 	
 	public Point getShipLocation() {
@@ -42,7 +52,7 @@ public class PlayerShip extends Observable implements ShipInterface{
 
 	public void moveEast() {
 		//Checks if the ship is not already at the right edge of the map
-		if(location.x < oceanMap.dimension - 1) {
+		if(location.x < oceanMap.getDimension() - 1) {
 			//Checks if the space to the right is a water square
 			if(map[location.x + 1][location.y] == 0) {
 				//Moves the ship 1 square east
@@ -54,7 +64,7 @@ public class PlayerShip extends Observable implements ShipInterface{
 
 	public void moveSouth() {
 		//Checks if the ship is not already at the bottom edge of the map
-		if(location.y < oceanMap.dimension - 1) {
+		if(location.y < oceanMap.getDimension() - 1) {
 			//Checks if the space below is a water square
 			if(map[location.x][location.y + 1] == 0) {
 				//Moves the ship 1 square south
