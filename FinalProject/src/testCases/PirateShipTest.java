563323884257dcd59fc483790ade209c251e458d
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-03-01 02:09:53 -0600

    OceanMapTest
    Not currently working the way I intended

--- FinalProject/src/testCases/PirateShipTest.java
+++ FinalProject/src/testCases/PirateShipTest.java
@@ -7,6 +7,7 @@ import java.awt.Point;
 import org.junit.Test;
 
 public class PirateShipTest {
+	columbus.OceanMap oceanMap;
 
 	@Test
 	public void test() {
@@ -19,6 +20,22 @@ public class PirateShipTest {
 		pirate.moveEast();
 		Point loc = pirate.getShipLocation();
 		assertEquals(loc, new Point(1,0));
+	}
+	
+	//should probably be in its own separate unit case
+	@Test
+	public void OceanMapTest() {
+		int[][] oMap = oceanMap.getMap();
+		int dimension = oceanMap.getDimension();
+		boolean error = false;
+		for (int x = 0; x < dimension; x++) {
+			for (int y = 0; y < dimension; y++) {
+				if (oMap[x][y] < 0) {
+					error = true;
+				}
+			}
+		}
+		assertFalse(error);
 	}
 
 }
