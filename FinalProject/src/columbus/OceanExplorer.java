a0f97c4ea2b3f2d6814f5ab0c3540fa9b62dedef
jkash1 <rawrdpro@gmail.com>
2018-03-15 19:13:17 -0500

    Final Menu changes

--- FinalProject/src/columbus/OceanExplorer.java
+++ FinalProject/src/columbus/OceanExplorer.java
@@ -13,6 +13,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 //import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -36,12 +37,15 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -73,7 +77,8 @@ public class OceanExplorer extends Application{
 		//Gets an instance of the ocean map, gets its dimensions, and gets the map from it
 		menu = oceanStage;
 		Pane root1 = new Pane();
-
+		
+		
 		//creates Menu background image
 		Image colimg = new Image("/images/columbusship.jpg");
 		ImageView colimgview = new ImageView(colimg);
@@ -86,7 +91,8 @@ public class OceanExplorer extends Application{
 		//calls title class, creates title for menu
 		Title title = new Title("C O L U M B U S    S A I L S    T H E    D E E P   B L U E ");
 		title.setTranslateX(10);
-		title.setTranslateY(50);
+		title.setTranslateY(20);
+		
 		
 		//adds all menu items
 		root1.getChildren().addAll(colimgview, gamemenu, title);
@@ -139,13 +145,6 @@ public class OceanExplorer extends Application{
 			root.getChildren().add(rect);
 			healthBar.add(rect);
 		}
-		
-		//Sets the scene, adds a title to the window, and shows it
-	/*	oceanStage.setScene(scene2);
-		oceanStage.setTitle("Columbus vs. the Deep Blue");
-		oceanStage.show();
-		*/
-		
 		
 		//Spawns the monsters and starts the thread
 		monsterSpawner = new MonsterSpawner(20, this);
@@ -532,6 +531,42 @@ public class OceanExplorer extends Application{
 			menu1.setTranslateX(100);
 			menu1.setTranslateY(200);
 			
+			/*
+			 * sets the text for the main menu
+			 */
+			
+			Text textmain = new Text("Welcome to the Ocean Blue \n"
+								+ "                              \n"
+								+ "Find the treasure before the \n "
+								+ "pirates capture you and beware\n "
+								+ "the mighty leviathans of the deep"
+								);
+			textmain.setFill(Color.DARKBLUE);
+			textmain.setTranslateX(445);
+			textmain.setTranslateY(150);
+			textmain.setFont(Font.font("Serif", FontWeight.BOLD, FontPosture.ITALIC, 25));
+			
+			/*
+			 * Sets the text for the Controls menu
+			 */
+			
+			Text textOptions = new Text("W - move Up \n"
+										+"A - move Left\n"
+										+ "S - move Down\n"
+										+ "D - move Right\n"
+										+"------------------\n"
+										+"Squids and Sharks Deal\n"
+										+ "one damage upon attacking\n"
+										+ "avoid them at all costs"
+										);
+			textOptions.setFill(Color.BLACK);
+			textOptions.setTranslateX(525);
+			textOptions.setTranslateY(175);
+			textOptions.setFont(Font.font("Serif", FontWeight.BOLD, 20));
+			
+			
+			
+			
 			menu2.setTranslateX(100);
 			menu2.setTranslateY(200);
 			
@@ -558,18 +593,20 @@ public class OceanExplorer extends Application{
 			//Creates Option button in MainMenu
 			MenuButton btnOptions = new MenuButton("Options");
 			btnOptions.setOnMouseClicked(event -> {
-				getChildren().add(menu1);
+				getChildren().addAll(menu1);
 				TranslateTransition ot = new TranslateTransition(Duration.seconds(0.25), menu0);
 				ot.setToX(menu0.getTranslateX() - offset);
 				
 				TranslateTransition ot1 = new TranslateTransition(Duration.seconds(0.5), menu1);				
 				ot1.setToX(menu0.getTranslateX());
 				
+	
+				
 				ot.play();
 				ot1.play();
 			
 				ot.setOnFinished(evt -> {
-					getChildren().remove(menu0);
+					getChildren().removeAll(menu0, textmain);
 				});
 
 			});
@@ -593,7 +630,7 @@ public class OceanExplorer extends Application{
 			//Creates the back button in the options menu
 			MenuButton btnBack = new MenuButton("Go Back");
 			btnBack.setOnMouseClicked(event -> {
-				getChildren().add(menu0);
+				getChildren().addAll(menu0, textmain);
 				TranslateTransition ot = new TranslateTransition(Duration.seconds(0.25), menu1);
 				ot.setToX(menu1.getTranslateX() + offset);
 				
@@ -604,13 +641,13 @@ public class OceanExplorer extends Application{
 				ot1.play();
 				
 				ot.setOnFinished(evt -> {
-					getChildren().remove(menu1);
+					getChildren().removeAll(menu1);
 			});
 		});
 			
 			MenuButton btnControls = new MenuButton("Controls");
 			btnControls.setOnMouseClicked(event -> {
-				getChildren().add(menu2);
+				getChildren().addAll(menu2, textOptions);
 				TranslateTransition ot = new TranslateTransition(Duration.seconds(0.25), menu1);
 				ot.setToX(menu1.getTranslateX() - offset);
 			
@@ -621,7 +658,7 @@ public class OceanExplorer extends Application{
 				ot1.play();
 			
 				ot.setOnFinished(evt -> {
-					getChildren().remove(menu1);
+					getChildren().removeAll(menu1, textmain);
 		});
 	});
 			
@@ -631,18 +668,18 @@ public class OceanExplorer extends Application{
 			 */
 			MenuButton btnMain = new MenuButton("Go To Main Menu");
 			btnMain.setOnMouseClicked(event -> {
-				getChildren().add(menu0);
+				getChildren().addAll(menu0, textmain);
 				TranslateTransition ot = new TranslateTransition(Duration.seconds(0.25), menu2);
 				ot.setToX(menu2.getTranslateX() + offset);
 				
-				TranslateTransition ot1 = new TranslateTransition(Duration.seconds(0.5), menu0);				
+				TranslateTransition ot1 = new TranslateTransition(Duration.seconds(0.25), menu0);				
 				ot1.setToX(menu2.getTranslateX());
 				
 				ot.play();
 				ot1.play();
 				
 				ot.setOnFinished(evt -> {
-					getChildren().remove(menu2);
+					getChildren().removeAll(menu2,textOptions);
 			});
 		});
 			
@@ -659,7 +696,7 @@ public class OceanExplorer extends Application{
 				ot1.play();
 				
 				ot.setOnFinished(evt -> {
-					getChildren().remove(menu2);
+					getChildren().removeAll(menu2, textOptions);
 			});
 		});
 			
@@ -673,25 +710,25 @@ public class OceanExplorer extends Application{
 			menu1.getChildren().addAll(btnBack, btnControls, btnDifficulty);
 			menu2.getChildren().addAll(btnMain, btnBack2);
 			
-			Rectangle bg = new Rectangle(800,600);
+			Rectangle bg = new Rectangle(800,500);
 			bg.setFill(Color.GREY);
 			bg.setOpacity(0.4);
 			
-			getChildren().addAll(bg, menu0);
+			getChildren().addAll(bg, menu0, textmain);
 			
 		}
 	}
 	
 	public static class Title extends StackPane{
 		public Title(String name) {
-			Rectangle bg = new Rectangle(500,60);
+			Rectangle bg = new Rectangle(650,60);
 			bg.setStroke(Color.WHITE);
 			bg.setStrokeWidth(2);
 			bg.setFill(null);
 			
 			Text text = new Text(name);
 			text.setFill(Color.WHITE);
-			text.setFont(Font.font(20));
+			text.setFont(Font.font("Serif", 25));
 			
 			setAlignment(Pos.CENTER);
 			getChildren().addAll(bg,text);
